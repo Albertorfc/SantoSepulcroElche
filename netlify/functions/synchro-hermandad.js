da8ca06ca40f9a71dbda1347281b8bd3dc8ec41f
Albertorfc <111996304+Albertorfc@users.noreply.github.com>
2026-05-18 23:45:04 +0200

    Change Google Sheets API key in synchro-hermandad.js
    Updated API key for Google Sheets access.

--- netlify/functions/synchro-hermandad.js
+++ netlify/functions/synchro-hermandad.js
@@ -6,11 +6,11 @@ exports.handler = async (event, context) => {
   
   const SHEET_ID = '1hp_36PFo3y0draB20sSoBxgNFiyoFSr5QL7uyb2PzXE';
   const TAB_NAME = 'Miembros'; // Nombre de la pestaña
-  const API_KEY = 'TU_GOOGLE_SHEETS_API_KEY'; // Necesitas una de Google Cloud
+  const API_KEY = 'AIzaSyAl2JwBaQIWFvbanCMmFEUhKFEJsw5Df0c'; // Necesitas una de Google Cloud
   
   try {
     // 1. Leer datos de Google Sheets
-    const url = `https://sheets.googleapis.com/v4/spreadsheets/${1hp_36PFo3y0draB20sSoBxgNFiyoFSr5QL7uyb2PzXE}/values/${Miembros}?key=${API_KEY}`;
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${1hp_36PFo3y0draB20sSoBxgNFiyoFSr5QL7uyb2PzXE}/values/${Miembros}?key=${AIzaSyAl2JwBaQIWFvbanCMmFEUhKFEJsw5Df0c}`;
     const response = await axios.get(url);
     const filas = response.data.values; // Esto nos da el array de hermanos
 
